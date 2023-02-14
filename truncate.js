543804d8c823cbe5e6cff4f2de78bd61e75c9a73
andrejsayan2006 <123058529+andrejsayan2006@users.noreply.github.com>
2023-02-14 14:13:22 +0700

    Update truncate.js

--- truncate.js
+++ truncate.js
@@ -2,4 +2,4 @@ const truncate = (text, length) => {
     return (text.length>length)?
         text.slice(0, length - 1) + "…" :text;
 };
-console.log(truncate("привет как дела?", 5))
+console.log(truncate("Я укорачиваю текст и ставлю ...", 5));
