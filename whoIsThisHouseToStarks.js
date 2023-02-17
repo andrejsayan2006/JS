575e6cf0ec508439a7240e4789a04ac11bc7fae5
andrejsayan2006 <123058529+andrejsayan2006@users.noreply.github.com>
2023-02-17 10:42:49 +0700

    Update whoIsThisHouseToStarks.js

--- whoIsThisHouseToStarks.js
+++ whoIsThisHouseToStarks.js
@@ -1,12 +1,10 @@
 /*На электронной карте Вестероса, которую реализовал Сэм, союзники Старков отображены зеленым кружком, враги — красным, а нейтральные семьи — серым.
-
 Напишите для Сэма функцию whoIsThisHouseToStarks(), которая принимает на вход фамилию семьи и возвращает одно из трёх значений: 'friend', 'enemy', 'neutral'.
-
 Правила определения:
-
 Друзья ('friend'): 'Karstark', 'Tally'
 Враги ('enemy'): 'Lannister', 'Frey'
 Любые другие семьи считаются нейтральными*/
+
 const whoIsThisHouseToStarks = (sentence) => {
 const lastChar = sentence;
 let sentenceType;
