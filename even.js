1f0e17baf283991b1a6ba7d07568d190dd0e96ba
andrejsayan2006 <123058529+andrejsayan2006@users.noreply.github.com>
2023-02-21 14:13:10 +0700

    Create even.js

---
+++ even.js
@@ -0,0 +1,12 @@
+/*Реализуйте функцию even(), которая возвращает новую строку, состоящую из чётных символов исходной строки.*/
+const even = (str) => {
+  let i = 0;
+  let result = '';
+  while (i < str.length) {
+    if (i % 2 !== 0) {
+      result = `${result}${str[i]}`;
+    }
+    i = i + 1;
+  }
+  return result;
+};
