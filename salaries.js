568c8abeb8e479290611a4162e952f9ee159089e
andrejsayan2006 <123058529+andrejsayan2006@users.noreply.github.com>
2023-03-23 13:54:33 +0700

    Update salaries.js

--- salaries.js
+++ salaries.js
@@ -1,11 +1,18 @@
-/*Напишите код для суммирования всех зарплат и сохраните результат в переменной sum*/
+/*Напишите код для суммирования всех зарплат и сохраните результат в переменной sum
+Добавил от себя вывод, если одно из полей не содержит цифры*/
 let salaries = {
-    Vasya: 20000,
-    Anna: 18500,
-    Petka: 35000
+    Vasya: 'aaaa',
+    Anna: 'bbbb',
+    Petka: 'cccc',
 };
 let sum = 0;
-for (let key in salaries){
-    sum +=salaries[key];
+let errorSymbol = 0;
+for (let key in salaries) {
+    if (typeof salaries[key] !== 'number') {
+        errorSymbol = (`Ключ: ${key} cодержит не число, а следующий символ: ${salaries[key]}`);
+        console.log(`Не верные значения в одном из свойств объекта, суммирование его пропустило! \n${errorSymbol}`);
+    } else if (typeof salaries[key] == 'number') {
+        sum += salaries[key];
+    }
 }
-console.log(sum);
+console.log(`Сумма значений составила: ${sum}`);
