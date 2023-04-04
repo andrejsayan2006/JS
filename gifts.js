282ab5ee80e8c6ba974d874b44bf6a95268abefb
andrejsayan2006 <123058529+andrejsayan2006@users.noreply.github.com>
2023-04-04 14:47:38 +0700

    Update gifts.js

--- gifts.js
+++ gifts.js
@@ -14,21 +14,17 @@ let gifts = [
         { name: 'Toy Car', id: 8, price: 25},
         { name: 'Basketball', id: 9, price: 20},
         { name: 'Scary Mask', id: 10, price: 75}
-]
+];
+function listOfGifts() {
+        for (key of gifts) {
+                console.log(`${key.id}- ${key.name}, Cost: ${key.price} tickets`);
+        }
+}
 console.log (`WELCOME TO THE CARNIVAL GIFT SHOP!
 Hello friend! Thank you for visiting the carnival!
 Here's the list of gifts:
-
-${gifts[0].id}- ${gifts[0].name}, Cost: ${gifts[0].price} tickets
-${gifts[1].id}- ${gifts[1].name}, Cost: ${gifts[1].price} tickets
-${gifts[2].id}- ${gifts[2].name}, Cost: ${gifts[2].price} tickets
-${gifts[3].id}- ${gifts[3].name}, Cost: ${gifts[3].price} tickets
-${gifts[4].id}- ${gifts[4].name}, Cost: ${gifts[4].price} tickets
-${gifts[5].id}- ${gifts[5].name}, Cost: ${gifts[5].price} tickets
-${gifts[6].id}- ${gifts[6].name}, Cost: ${gifts[6].price} tickets
-${gifts[7].id}- ${gifts[7].name}, Cost: ${gifts[7].price} tickets
-${gifts[8].id}- ${gifts[8].name}, Cost: ${gifts[8].price} tickets
-${gifts[9].id}- ${gifts[9].name}, Cost: ${gifts[9].price} tickets`);
+`)
+listOfGifts();
 
 
 
