73dc05a4a2702121e6966f5bd5eafdd943e27643
andrejsayan2006 <123058529+andrejsayan2006@users.noreply.github.com>
2023-04-28 11:09:39 +0700

    Create object.js

---
+++ object.js
@@ -0,0 +1,9 @@
+/*Напишите конструктор функции, вызываемый Phone со свойствами name, height, width, screenSizeи resolution.*/
+function Phone (name, height, width, screenSize, resolution) {
+  this.name = name;
+  this.height = height;
+  this.width = width;
+  this.screenSize = screenSize;
+  this.resolution = resolution;
+  return;
+}
