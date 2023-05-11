bc23dddc8bc17c1c07bd12f9cc9f2a9d88e77ae0
andrejsayan2006 <123058529+andrejsayan2006@users.noreply.github.com>
2023-05-11 16:02:17 +0700

    Update hyperskillBot.js

--- hyperskillBot.js
+++ hyperskillBot.js
@@ -1,51 +1,56 @@
 const input = require('sync-input');
 
-console.log("Hello! My name is Aid.")
-console.log("I was created in 2020.")
-console.log("Please, remind me your name.")
-
-let name = input()
-
-console.log("What a great name you have, " + name + "!")
-console.log("Let me guess your age.")
-console.log("Enter remainders of dividing your age by 3, 5 and 7.")
-
-let rem3 = Number(input());
-let rem5 = Number(input());
-let rem7 = Number(input());
-
-let age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
-
-console.log("Your age is " + age + "; that's a good time to start programming!");
-console.log("Now I will prove to you that I can count to any number you want.");
-
-const number = input();
-for (i=0; i<=number; i++){
-    console.log(`${i} !`)
+function userHello (bot_name, birth_year) {
+    console.log (`Hello! My name is ${bot_name}.
+I was created in ${birth_year}.`);
 }
-console.log(`Let's test your programming knowledge.
+
+function userName () {
+    console.log("Please, remind me your name.");
+    let name = input();
+    console.log(`What a great name you have, ${name}!`);
+    }
+
+function userAge () {
+    console.log(`Let me guess your age.
+Enter remainders of dividing your age by 3, 5 and 7.`);
+    let rem3 = Number(input());
+    let rem5 = Number(input());
+    let rem7 = Number(input());
+
+    let age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+    console.log(`Your age is ${age}; that's a good time to start programming!`);
+    }
+
+function count () {
+    console.log("Now I will prove to you that I can count to any number you want.");
+    const number = input();
+    for (let i=0; i<=number; i++){
+        console.log(`${i} !`);
+    }
+}
+
+function userTest () {
+    console.log(`Let's test your programming knowledge.
 Why do we use methods?
 1. To repeat a statement multiple times.
 2. To decompose a program into several small subroutines.
 3. To determine the execution time of a program.
-4. To interrupt the execution of a program.`)
+4. To interrupt the execution of a program.`);
 
-let choice = '2';
-do {choice = input();
-    if (choice !== 2) {
-        switch (choice) {
-            case '1':
-            case '3':
-            case '4':
-                console.log ("Please, try again.");
-                break;
-            case '2':
-                console.log("Congratulations, have a nice day!")
-                break;
-            default:
-                console.log('Invalid choice');
-        }
+    let question = input();
+    while (question !=2) {
+        console.log("Please, try again.");
+        question = input();
     }
-    else console.log (`Please enter a valid number!`);
 }
-while (choice !== "2");
+function end() {
+    console.log("Completed, have a nice day!");
+    console.log("Congratulations, have a nice day!");
+}
+userHello('Aid', '2020')  // change it as you need
+userName();
+userAge();
+count();
+userTest();
+end();
